9b3619fed716a42df53e9dcceae73e06d3615a93
Sachin-2006 <sachin06kaizen@gmail.com>
2025-03-12 10:01:37 +0530

    worked on components

---
+++ src/components/usercard.jsx
@@ -0,0 +1,17 @@
+// write the component code here
+import React from 'react'
+
+function usercard() {
+  return (
+    <div style={{border:"3px",borderColor:"white"}}>
+        <img src="https://media.gq.com/photos/650b335c6bd96a9cdafbcd2a/1:1/w_1125,h_1125,c_limit/diddy.jpg" alt="" style={{height:"100px"}}/>
+        <h4>Diddy .P</h4>
+        <h5>nicetrydiddy@baby.oil</h5>
+        <h3>Phone:6969696969</h3>
+        Address:🏠 123 Can't Hide Forever Blvd
+📍 Nice Try, Diddy, NY 00000
+    </div>
+  )
+}
+
+export default usercard
