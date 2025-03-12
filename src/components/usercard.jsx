819e29b94b0d96d1ca7097458446cc65cf74f979
laser <72447101+laser069@users.noreply.github.com>
2025-03-12 10:07:48 +0530

    Update usercard.jsx

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -2,14 +2,19 @@
 import React from 'react'
 
 function usercard() {
+  const email = "nicetrydiddy@baby.oil";
+  const name = "Diddy .P"
+  const imgurl = "https://media.gq.com/photos/650b335c6bd96a9cdafbcd2a/1:1/w_1125,h_1125,c_limit/diddy.jpg"
+  const phone = "6969696969"
+  const address = `🏠 123 Can't Hide Forever Blvd
+📍 Nice Try, Diddy, NY 00000`
   return (
     <div style={{border:"3px",borderColor:"white"}}>
-        <img src="https://media.gq.com/photos/650b335c6bd96a9cdafbcd2a/1:1/w_1125,h_1125,c_limit/diddy.jpg" alt="" style={{height:"100px"}}/>
-        <h4>Diddy .P</h4>
-        <h5>nicetrydiddy@baby.oil</h5>
-        <h3>Phone:6969696969</h3>
-        Address:🏠 123 Can't Hide Forever Blvd
-📍 Nice Try, Diddy, NY 00000
+        <img src={imgurl} alt="" style={{height:"100px"}}/>
+        <h4>{name}</h4>
+        <h5>{email}</h5>
+        <h3>Phone:{phone}</h3>
+        Address:{address}
     </div>
   )
 }
